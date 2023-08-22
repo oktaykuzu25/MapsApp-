b49ce350fe013049577b58f82fced5ebff2b5569
oktaykuzu <oktaykuzu00@gmail.com>
2023-08-22 16:24:06 +0300

    Initial Commit

---
+++ HaritaUygulamasi/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HaritaUygulamasi
+//
+//  Created by Oktay Kuzu on 22.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
